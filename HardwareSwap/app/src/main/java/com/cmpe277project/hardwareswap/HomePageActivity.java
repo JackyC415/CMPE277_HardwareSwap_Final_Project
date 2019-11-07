770662823f28b1815d97d1dbc1b5c9057f7a5357
Fuong <fuongbregas@icloud.com>
2019-11-07 12:54:27 -0800

    Connect to Firebase

--- HardwareSwap/app/src/main/java/com/cmpe277project/hardwareswap/HomePageActivity.java
+++ HardwareSwap/app/src/main/java/com/cmpe277project/hardwareswap/HomePageActivity.java
@@ -4,12 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class HomePageActivity extends AppCompatActivity {
+
+    private static FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
+
     }
 
 
