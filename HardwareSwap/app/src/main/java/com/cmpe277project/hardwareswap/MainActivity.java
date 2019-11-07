e909b55012dee1b1446987c169a45b50fd62e6b1
Fuong <fuongbregas@icloud.com>
2019-11-06 21:07:00 -0800

    Button
    buttons

--- HardwareSwap/app/src/main/java/com/cmpe277project/hardwareswap/MainActivity.java
+++ HardwareSwap/app/src/main/java/com/cmpe277project/hardwareswap/MainActivity.java
@@ -3,6 +3,11 @@ package com.cmpe277project.hardwareswap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +15,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final Button loginButton = findViewById(R.id.loginButton);
+        final Button registerButton = findViewById(R.id.registerButton);
+        final TextView phoneNumber = findViewById(R.id.phoneNumber);
+        final TextView password = findViewById(R.id.password);
+
+        // Set onClickListener
+        final View.OnClickListener buttonClick = new View.OnClickListener(){
+            public void onClick(View view){
+                final int id = view.getId();
+
+                if(id == R.id.loginButton){
+                    System.out.println("Login Button Clicked");
+
+                }
+                else if (id == R.id.registerButton){
+                    System.out.println("Register Button Clicked");
+                }
+            }
+        };
+
+        // Set button click
+        loginButton.setOnClickListener(buttonClick);
+        registerButton.setOnClickListener(buttonClick);
+
     }
+
+
 
 
 }
